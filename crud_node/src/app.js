cb62013e5e4e431385fa023e67079ebfc53ecda0
roussita17 <rosita_gv17@hotmail.com>
2019-04-29 13:59:24 -0600

    Conexion a la base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const mysql = require ('mysql');
+const myConnection = require('express-myconnection');
 const app = express(); // le asignamos express a la app
 
 
@@ -6,6 +9,20 @@ const app = express(); // le asignamos express a la app
 //Importamos las rutas a traves de una constante
 const indiceRutas = require('./rutas/index');
 
+//Configuraciones
+app.set('port', process.env.PORT || 3000); //env = variable de ambiente , process...toma el puerto del SO
+app.set('view engine', 'ejs'); //view engine = motor de vistas (ejs)
+app.set('views', path.join(__dirname,'views'));
+
+
+//Middleware
+app.use(myConnection(mysql,{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudenodemysql13'
+},'single'));
 
 //Establecer y usar las rutas 
 app.use("/", indiceRutas);

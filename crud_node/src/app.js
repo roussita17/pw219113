54c26568a3edfab4a6999e7fd8a8e2acca2a7bea
roussita17 <rosita_gv17@hotmail.com>
2019-04-30 13:58:26 -0600

    Listado de clientes y controlador

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const path = require('path');
-const mysql = require ('mysql');
-const myConnection = require('express-myconnection');
 const app = express(); // le asignamos express a la app
+const mysql = require('mysql');
+const myConnection = require ('express-myconnection');
 
 
 
 //Importamos las rutas a traves de una constante
 const indiceRutas = require('./rutas/index');
 
+
 //Configuraciones
-app.set('port', process.env.PORT || 3000); //env = variable de ambiente , process...toma el puerto del SO
-app.set('view engine', 'ejs'); //view engine = motor de vistas (ejs)
-app.set('views', path.join(__dirname,'views'));
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
 
 
-//Middleware
+//middleware
 app.use(myConnection(mysql,{
 	host: 'localhost',
 	user: 'root',
 	password: '',
 	port: 3306,
 	database: 'crudenodemysql13'
-},'single'));
+}, 'single'));
+
 
 //Establecer y usar las rutas 
 app.use("/", indiceRutas);
--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,18 @@
 const express = require ('express');
 const rutas = express.Router();
 
-rutas.get("/", (req,res) => {
-	res.send("hola mundo en node jajaja");
-});
+const customerController = require('../controllers/customerController');
+
+//rutas.get("/", (req,res) => {
+//	res.send("hola mundo en node jajaja");
+//});
+
+rutas.get('/', customerController.list);
+//rutas.post('/add', customerController.save);
+//rutas.get('/delete', customerController.delete);
+//rutas.get('/update/:id', customerController.edit);  //pantalla de edicion, get manda llamar un apantalla
+//rutas.post('/update/:id', customerController.update); // post = envia datos 
+
+
 
 module.exports = rutas;

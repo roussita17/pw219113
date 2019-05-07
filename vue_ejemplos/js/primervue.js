2273fe066d0894d7e10dc09bac1fa22896ba8af4
roussita17 <rosita_gv17@hotmail.com>
2019-05-07 13:58:23 -0600

    Practica1-Vue.js

---
+++ vue_ejemplos/js/primervue.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		titulo: 'hola mundo en Vue',
+		frutas: ['Manzana','Uvas','Peras','Fresas'],
+		frutas2: [
+		   {nombre:'Durazno', Cantidad: 11},
+		   {nombre:'Guayaba', Cantidad: 0},
+		   {nombre:'Frambuesa',Cantidad: 80},
+		   {nombre:'arandano',Cantidad: 110}
+
+		]
+	}
+});
